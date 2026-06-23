04b5df02df008e5c19fd79a167cd7cc30da97a77
Yeboah6 <ads21b00223y@ait.edu.gh>
2026-06-23 14:35:44 +0000

    Start

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+             input: ['resources/js/app.jsx'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
+
+// import { defineConfig } from 'vite'
+// import laravel from 'laravel-vite-plugin'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/js/app.jsx'],
+//             refresh: true,
+//         }),
+//         react(),
+//     ],
+// })
